56165412799f0c3d5427b0baf00efec79a64089e
David Ortiz <dkaortiz@gmail.com>
2023-12-05 10:59:39 -0300

    Aula2_3

--- src/pages/Cardapio/index.tsx
+++ src/pages/Cardapio/index.tsx
@@ -3,9 +3,8 @@ import styles from './Cardapio.module.scss'; //importar estilo como objeto
 export default function Cardapio(){
     return(
         <main>
-            <nav className="menu">
-                
-
+            <nav className={styles.menu}>
+                <img src='../../assets/logo.svg' alt='Logo do aluroni' />
             </nav>
         </main>
     )
