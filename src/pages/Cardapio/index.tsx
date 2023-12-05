fcb9c7583a7bacfee98835e764f44158f35522ad
David Ortiz <dkaortiz@gmail.com>
2023-12-05 15:37:43 -0300

    Aula3_5 Header

--- src/pages/Cardapio/index.tsx
+++ src/pages/Cardapio/index.tsx
@@ -8,6 +8,12 @@ export default function Cardapio(){
                <Logo />
                
             </nav>
+            <header className={styles.header}>
+                <div className={styles.header__text}>
+                    A casa do codigo e da massa
+
+                </div>
+            </header>
         </main>
     )
 }
