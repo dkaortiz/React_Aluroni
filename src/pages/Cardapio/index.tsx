b3950ca19c0ad1128bb12c3618fe401685e6fe3a
David Ortiz <dkaortiz@gmail.com>
2023-12-05 10:41:58 -0300

    Aula2_1

---
+++ src/pages/Cardapio/index.tsx
@@ -0,0 +1,12 @@
+import styles from './Cardapio.module.scss'; //importar estilo como objeto
+
+export default function Cardapio(){
+    return(
+        <main>
+            <nav className="menu">
+                
+
+            </nav>
+        </main>
+    )
+}
