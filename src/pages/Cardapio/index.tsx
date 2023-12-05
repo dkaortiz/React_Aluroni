31bbf082e7faf7bdacf813a8c4fcba09bed40fa9
David Ortiz <dkaortiz@gmail.com>
2023-12-05 11:15:57 -0300

    Aula2_4 Importando Logo

--- src/pages/Cardapio/index.tsx
+++ src/pages/Cardapio/index.tsx
@@ -1,10 +1,11 @@
 import styles from './Cardapio.module.scss'; //importar estilo como objeto
+import {ReactComponent as Logo} from '../../assets/logo.svg';
 
 export default function Cardapio(){
     return(
         <main>
             <nav className={styles.menu}>
-                <img src='../../assets/logo.svg' alt='Logo Aluroni' />
+               <Logo />
             </nav>
         </main>
     )
